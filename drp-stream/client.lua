951b0d49f1088b941db0a07bf5cf4c9837ad0a8b
Ciemniak <43303565+Ciemniakk@users.noreply.github.com>
2021-01-12 23:36:32 +0100

    Update client.lua

--- drp-stream/client.lua
+++ drp-stream/client.lua
@@ -1,4 +1,19 @@
 Citizen.CreateThread(function()
+    SetMapZoomDataLevel(0, 0.96, 0.9, 0.08, 0.0, 0.0)
+    SetMapZoomDataLevel(1, 1.6, 0.9, 0.08, 0.0, 0.0)
+    SetMapZoomDataLevel(2, 8.6, 0.9, 0.08, 0.0, 0.0)
+    SetMapZoomDataLevel(3, 12.3, 0.9, 0.08, 0.0, 0.0)
+    SetMapZoomDataLevel(4, 22.3, 0.9, 0.08, 0.0, 0.0)
+end)
+
+Citizen.CreateThread(function()
+    while true do
+	Citizen.Wait(1)
+	SetRadarZoom(1100)
+    end
+end)
+
+Citizen.CreateThread(function()
 	
     RequestIpl("rcustoms_milo")
 	RequestIpl("hei_dlc_vw_roofdoors_locked")
